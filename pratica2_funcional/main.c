0013f99785bdfb475399985462f3cde5b5aa2578
ayubero <32571894+ayubero@users.noreply.github.com>
2023-10-21 20:36:00 +0200

    practica2

--- pratica2_funcional/main.c
+++ pratica2_funcional/main.c
@@ -8,46 +8,46 @@
 
 // Nota: wait es una espera activa. Se puede eliminar poniendo el procesador en modo iddle. Probad a hacerlo
 void wait (void)  {                         /* wait function */
-  unsigned int i;
+	unsigned int i;
 
-  i = timer0_read_int_count(); // reads the number of previous timer IRQs
-  while ((i + 10) != timer0_read_int_count());  /* waits for 10 interrupts, i.e. 50ms */
+	i = timer0_read_int_count(); // reads the number of previous timer IRQs
+	while ((i + 10) != timer0_read_int_count());  /* waits for 10 interrupts, i.e. 50ms */
 }
 
 int main (void) {
-  unsigned int j;                           /* LED var */
+ 	unsigned int j; /* LED var */
 	unsigned int total;
 	eint0_init(); // activates EINT0 interrupts
-	// Nota la gestión del GPIO vosotros la debeís hacer en GPIO.c no en el main o en el reversi
+	// Nota la gestiï¿½n del GPIO vosotros la debeï¿½s hacer en GPIO.c no en el main o en el reversi
 	//IODIR 		= 0x00FF0000;					//Set LED pins as outputs 
 	gpio_hal_sentido(16, 8,GPIO_HAL_PIN_DIR_OUTPUT);
-	IOCLR 		= 0x00FF0000;					//Initialices the outputs to 0
+	IOCLR = 0x00FF0000;					//Initialices the outputs to 0
 
-	// bucle para comprobar el funcionamiento del botón. El objetivo es comprobar que se lleva bien la cuenta de pulsaciones
-	// con sólo una interrupción EXTINT0 por pulsación
-	// en este proyecto no va a funcionar porque la interrupción se activa por nivel y no se ha añadido la gestión necesaria para ue sólo interrumpa una vez.
+	// bucle para comprobar el funcionamiento del botï¿½n. El objetivo es comprobar que se lleva bien la cuenta de pulsaciones
+	// con sï¿½lo una interrupciï¿½n EXTINT0 por pulsaciï¿½n
+	// en este proyecto no va a funcionar porque la interrupciï¿½n se activa por nivel y no se ha aï¿½adido la gestiï¿½n necesaria para ue sï¿½lo interrumpa una vez.
 	while (eint0_read_count() < 4){
-		PM_power_down(); // de aquí sólo despertamos si hay pulsación
-		};	
-// bucle que realiza un blink de leds cada 50ms	   
+		PM_power_down(); // de aquï¿½ sï¿½lo despertamos si hay pulsaciï¿½n
+	};	
+	// bucle que realiza un blink de leds cada 50ms	   
 	temporizador_hal_iniciar(); // generates an interrupt every 0,05ms and increments timeval0
 	temporizador_hal_empezar();
 	
 	//PRUEBAS	
 
 while (1)  {  
-		int pin16=16;
-		for (pin16 = 16; pin16 < 24; pin16++){
-					gpio_hal_escribir(pin16,1,1);
-					wait();
-					gpio_hal_escribir(pin16,1,0);
-		}	
-		for(pin16=23; pin16 >= 16; pin16--){
-					gpio_hal_escribir(pin16,1,1);
-					wait();
-					gpio_hal_escribir(pin16,1,0);
-		}
-		total=temporizador_hal_parar();
+	int pin16=16;
+	for (pin16 = 16; pin16 < 24; pin16++){
+		gpio_hal_escribir(pin16,1,1);
+		wait();
+		gpio_hal_escribir(pin16,1,0);
+	}	
+	for(pin16=23; pin16 >= 16; pin16--){
+		gpio_hal_escribir(pin16,1,1);
+		wait();
+		gpio_hal_escribir(pin16,1,0);
+	}
+	total = temporizador_hal_parar();
 		
 }
 
@@ -56,13 +56,13 @@ while (1)  {
 //while (1){
 		/* Loop forever */
 //    for (j = 0x010000; j < 0x800000; j <<= 1) { /* Blink LED 0,1,2,3,4,5,6 */
-      // Nota la gestión del GPIO vosotros la debeís hacer en GPIO.c no en el main o en el reversi
+      // Nota la gestiï¿½n del GPIO vosotros la debeï¿½s hacer en GPIO.c no en el main o en el reversi
 //			IOSET = j;                               /* Turn on LED */
 //      wait ();                                  /* call wait function */
 //      IOCLR = j;                               /* Turn off LED */
     }
  //   for (j = 0x800000; j > 0x010000; j >>=1 ) { /* Blink LED 7,6,5,4,3,2,1 */
-      // Nota la gestión del GPIO vosotros la debeís hacer en GPIO.c no en el main o en el reversi
+      // Nota la gestiï¿½n del GPIO vosotros la debeï¿½s hacer en GPIO.c no en el main o en el reversi
 //			IOSET = j;                               /* Turn on LED */
  //     wait ();                                  /* call wait function */
 //     IOCLR = j;                               /* Turn off LED */
